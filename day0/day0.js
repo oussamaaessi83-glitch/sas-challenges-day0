a42453315060db99e2a0f496e4f2a44c9427a492
oussama <oussamaaessi83@gmail.com>
2026-09-01 12:26:20 +0200

    const.js

--- day0/day0.js
+++ day0/day0.js
@@ -1,21 +1,47 @@
-let name = "oussama";
-let familyName = "el aissi";
-let age = 18;
-let studied_Situation = "student";
-let hobbies = "programing";
-let city = "youssoufia";
-let country = "moroco";
-let bacculaureate = "science humaines";
-let school = "Youcode"; 
+console.log("bac notes")
+const frensh = 14;
+const arabic = 19 ;
+const english = 17;
+const maths = 19;
+const sport = 18;
+const informatique = 20;
+const islamiqueEducation = 20;
+const historique = 18;
+const geographic = 18;
 
 
-console.log("Name: " + name);
-console.log("Family Name: " + familyName);
-console.log("Age: " + age);
-console.log("Studied Situation: " + studied_Situation);
-console.log("hobbies: " + hobbies);
-console.log("city : " + city);
-console.log("country: " + country);
-console.log("bacculaureate: " + bacculaureate);
-console.log("School : " + school) ;
 
+console.log("Frensh Note : " + frensh);
+console.log("arabic Note : " + arabic);
+console.log("english Note : " + english);
+console.log("maths Note : " + maths);
+console.log("sport Note : " + sport);
+console.log("informatique Note : " + informatique);
+console.log("historique Note : " + historique);
+console.log("geographic Note : " + geographic);
+
+
+
+
+
+
+
+const isstuden = true;
+const isman = true;
+const isbacstudent = true;
+const isdeveloper = true;
+const is_inyoucode = true;
+const is_has_license = false;
+const is_has_master = false;
+const is_has_achievements = true;
+
+
+
+console.log("student : " + isstuden);
+console.log("man : " + isman );
+console.log("bac student : " + isbacstudent);
+console.log("developer : " + isdeveloper);
+console.log("youcoder : " + is_inyoucode);
+console.log("has license : " + is_has_license);
+console.log("has master : " + is_has_master);;
+console.log("has achaivements : " + is_has_achievements);
