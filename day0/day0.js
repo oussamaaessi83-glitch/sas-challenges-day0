cfb3f16d1434d0f128e1bc7b35e09d1d15d1b0f8
oussama <oussamaaessi83@gmail.com>
2026-09-01 14:25:22 +0200

    conditions

--- day0/day0.js
+++ day0/day0.js
@@ -1,24 +1,52 @@
-console.log("bac notes")
-const frensh = 14;
-const arabic = 19 ;
-const english = 17;
-const maths = 19;
-const sport = 18;
-const informatique = 20;
-const islamiqueEducation = 20;
-const historique = 18;
-const geographic = 18;
+// simple conditions
+
+// numbers conditions
+console.log("numbres conditions")
+let age1 = 18;
+if(age1 >= 20){console.log("ok");}
+else(age1 < 20)
+{console.log("the number is < 20");}
+
+
+let age2 = 11;
+if(age2 == 9){console.log("it is  9");}
+else if(age2 == 10){console.log("this is 10");}
+  
+else{console.log("it is not 10 OR 9");}
 
 
 
-console.log("Frensh Note : " + frensh);
-console.log("arabic Note : " + arabic);
-console.log("english Note : " + english);
-console.log("maths Note : " + maths);
-console.log("sport Note : " + sport);
-console.log("informatique Note : " + informatique);
-console.log("historique Note : " + historique);
-console.log("geographic Note : " + geographic);
+// string conditions
+console.log("string conditions")
+let name = "oussama";
+if(name="oussama"){console.log("hi " + name);}
+else if(name = "aessi"){console.log("welcome " + name);}
+else{console.log("sorry,you are not oussam or aessi");}
+
+
+// null conditions
+console.log("null conditions")
+const year = null;
+if( typeof year == null){console.log("type of variable is  " + year0 );}
+else if(typeof year != null){console.log("ok it not a null");}
+else {console.log("the type of var is  " + typeof year);}
+
+// complex conditions
+console.log("complex conditions ")
+console.log("complex")
+let x = 12;
+if(x == 12 || x == 11){
+    if(x == 12){console.log("ok x equal " + x);}
+    else if(x == 11){console.log("ok w equal "+ x);}
+    else{console.log("sorry this is error")}
+;}
+else if(x == 10 || x == 9  ){
+    if(x == 10){console.log("ok x equal " + x);}
+    else if(x == 11){console.log("ok w equal "+ x);}
+    else{console.log("sorry this is error");}
+
+;}
+else{console.log("x dosen't equal 9 OR 10 OR 11 OR 12 ");}
 
 
 
@@ -26,22 +54,4 @@ console.log("geographic Note : " + geographic);
 
 
 
-const isstuden = true;
-const isman = true;
-const isbacstudent = true;
-const isdeveloper = true;
-const is_inyoucode = true;
-const is_has_license = false;
-const is_has_master = false;
-const is_has_achievements = true;
 
-
-
-console.log("student : " + isstuden);
-console.log("man : " + isman );
-console.log("bac student : " + isbacstudent);
-console.log("developer : " + isdeveloper);
-console.log("youcoder : " + is_inyoucode);
-console.log("has license : " + is_has_license);
-console.log("has master : " + is_has_master);;
-console.log("has achaivements : " + is_has_achievements);
---
+++ test.js
@@ -0,0 +1,5 @@
+// null conditions
+const year = null;
+if( typeof year == null){console.log("type of variable is  " + year0 );}
+else if(typeof year != null ){console.log("ok it not a null");}
+else {console.log("the type of var is  " + typeof year);}
