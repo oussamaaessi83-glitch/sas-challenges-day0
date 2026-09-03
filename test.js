2e8247b66b0916a970e955fac92758dfda97bb5e
EL AISSI OUSSAMA <oussamaaessi83@gmail.com>
2026-09-03 13:54:04 +0200

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-// null conditions
-const year = null;
-if( typeof year == null){console.log("type of variable is  " + year0 );}
-else if(typeof year != null ){console.log("ok it not a null");}
-else {console.log("the type of var is  " + typeof year);}
