dad6937167b3ff03627d98b6055e1dcfab72e190
oussama <oussamaaessi83@gmail.com>
2026-09-03 13:36:40 +0200

    challenges

---
+++ challenges.js
@@ -0,0 +1,97 @@
+// level 1
+
+// 1 age logger :
+const age = 18;
+console.log("you are " + age +  "  years old");
+
+// 2 temperature Check:
+let tempreature = 29;
+if(tempreature > 30){console.log("it's hot outside !");}
+else{console.log("The weather is nice.");}
+
+// 3 Even or Odd:
+let number = 7;
+if(number % 2 == 0){console.log("this number is even");}
+else{console.log("this is odd");}
+
+
+
+// Level 2 (Intermediate)
+
+// Grading System 1
+let Grading_number = 77;
+if(Grading_number >= 90 && Grading_number <= 100 ){console.log("A");}
+else if(Grading_number >= 80 && Grading_number <= 89){console.log("B");}
+else if(Grading_number >= 70 && Grading_number <= 79){console.log("C");}
+else if( Grading_number <= 70){console.log("F");}
+else{console.log("this number is not on the list")}
+
+// Max of tree 2
+let number1 = 12;
+let number2 = 4;
+let number3 = 7;
+
+if(number1 > number2 && number1 > number3){console.log("the number max is  " + number1);}
+ else if(number2 > number1 && number2 > number3){console.log("the number max is  " + number2);}
+ else {console.log("the number max is  " + number3);}
+
+// Leap Year Checker 3 : 
+let year = 2022;
+if(year % 4 == 0 && year / 400 == 0){console.log("this is a leap year ! ");}
+else if (year != 100){console.log("this is a leap year ! ");}
+else{console.log("this not a leap year ");}
+
+
+
+
+
+
+
+
+
+
+// level advanced 3
+
+// basic calculator 1
+let number_1 = 8;
+let number_2 = 7;
+
+
+let operator = 1;
+switch ( ) {
+    case number1 + :
+        
+        break;
+
+    default:
+        break;
+}
+
+// Valid Triangle 3
+let a = 40;
+let b =60;
+let c = 80;
+
+if(a != 0 && b != 0 && c != 0){
+    if(a + c + b  == 180){console.log("the soome of angles is 180 ");}
+    else{console.log("the soome of angles is not 180 ");}
+
+;}
+else {console.log("there a angle equal 0")  }
+
+
+
+
+// BMI Calcualtor
+let weight = 60 ;
+let height = 1.72;
+let BMI = (weight / height * height)
+
+if(BMI < 18.5 ){console.log("Underweight")}
+else if (BMI >= 18.5 && 24.9){console.log("Normal")}
+else if (BMI >= 25){console.log("Normal")}
+
+
+
+
+
--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-// null conditions
-const year = null;
-if( typeof year == null){console.log("type of variable is  " + year0 );}
-else if(typeof year != null ){console.log("ok it not a null");}
-else {console.log("the type of var is  " + typeof year);}
+
+// console.log("      node .\test.js             ")
+
